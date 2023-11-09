541c27070eaa7bcbe3ae1763e31d55f40d750279
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-11-08 22:26:03 -0500

    Estructura de directorios y generando rutas en la capa de infraestructura

--- config/app.php
+++ config/app.php
@@ -168,6 +168,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * User Route Service Provider
+         */
+        Src\Example\User\Infrastructure\Services\RouteServiceProvider::class
+
     ])->toArray(),
 
     /*
